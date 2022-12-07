cc3bc600e95feded346602b89b8d19c50c88157f
Danny Verpoort <danny.verpoort@bouwatch.nl>
2022-12-07 01:07:39 +0100

    Solution to day 1

--- 01/main.go
+++ 01/main.go
@@ -1,9 +1,74 @@
 package main
 
 import (
+	"bufio"
+	"bytes"
 	"fmt"
+	"os"
+	"sort"
+	"strconv"
+	"strings"
 )
 
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
+func splitPerElf() func(data []byte, atEOF bool) (advance int, token []byte, err error) {
+	searchBytes := []byte("\n\n")
+	searchLength := len("\n\n")
+	return func(data []byte, atEOF bool) (advance int, token []byte, err error) {
+		dataLen := len(data)
+
+		if atEOF && dataLen == 0 {
+			return 0,nil,nil
+		}
+
+		if i := bytes.Index(data, searchBytes); i >= 0 {
+			return i + searchLength, data[0:i], nil
+		}
+
+		if atEOF {
+			return dataLen, data, nil
+		}
+
+		return 0, nil, nil
+	}
+}
+
 func main() {
-	fmt.Println("Hello World")
+	file, err := os.Open("./calories")
+
+	check(err)
+
+	defer func(file *os.File) {
+		err := file.Close()
+		check(err)
+	}(file)
+
+	scanner := bufio.NewScanner(file)
+
+	scanner.Split(splitPerElf())
+	check(scanner.Err())
+
+	var entities []int
+
+	for scanner.Scan() {
+		lines := strings.Fields(scanner.Text())
+		total := 0
+
+		for _, line := range lines {
+			entry, err := strconv.Atoi(line)
+			check(err)
+			total += entry
+		}
+
+		entities = append(entities, total)
+	}
+
+	sort.Ints(entities)
+	fmt.Println(entities[len(entities)-1])
+	fmt.Println(entities[len(entities)-1] + entities[len(entities)-2] + entities[len(entities)-3])
 }
